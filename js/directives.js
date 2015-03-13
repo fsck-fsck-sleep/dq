333a66d440a6119c0c3f1babf6d3e2dfb083f3be
fsckfscksleep <kfehw@inboxen.org>
2015-03-13 14:34:58 +0000

    Fix sidebar floating over questions
    Closes #1

--- js/directives.js
+++ js/directives.js
@@ -1,7 +1,7 @@
 dirtyQuestionsApp.directive('dqVisualisation', function () {
 	// constants
 	var margin = 20,
-		width = 300,
+		width = 220,
 		height = 500 - .5 - margin,
 		color = d3.interpolateRgb("#f77", "#77f");
 
